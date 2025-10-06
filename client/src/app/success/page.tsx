0a7c6aae679f4b4b0c948d8abd1e2e99343bfdd9
zehan12 <zehan9211@gmail.com>
2025-10-06 13:31:40 +0530

    fix: resolve z-index conflict with navigation bar

---
+++ client/src/app/success/page.tsx
@@ -0,0 +1,8 @@
+import SuccessPageClient from "@/components/pages/SuccessPageClient";
+import React from "react";
+
+const SuccessPage = () => {
+  return <SuccessPageClient />;
+};
+
+export default SuccessPage;
