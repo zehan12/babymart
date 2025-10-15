74d17b04d0ea8533be7d527f197acb9790e7820f
zehan12 <zehan9211@gmail.com>
2025-10-15 17:47:18 +0530

    feat: implement real-time shopping cart synchronization

---
+++ client/src/app/user/checkout/page.tsx
@@ -0,0 +1,8 @@
+import CheckoutPageClient from "@/components/pages/CheckoutPageClient";
+import React from "react";
+
+const CheckoutPage = () => {
+  return <CheckoutPageClient />;
+};
+
+export default CheckoutPage;
