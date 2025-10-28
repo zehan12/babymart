8688850f7f2e4464ead0a48c87acb79391802a2a
zehan12 <zehan9211@gmail.com>
2025-10-28 15:17:16 +0530

    perf: optimize database queries for product listing

---
+++ server/models/cartModel.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const cartItemSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+  image: {
+    type: String,
+  },
+});
+
+const cartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    items: [cartItemSchema],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Cart", cartSchema);
