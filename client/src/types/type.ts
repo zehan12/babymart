0d8edf0bb95d03a05327957f4092866186c24a08
zehan12 <zehan9211@gmail.com>
2025-11-17 19:37:08 +0530

    perf: implement image lazy loading and optimization

---
+++ client/src/types/type.ts
@@ -0,0 +1,54 @@
+export interface Category {
+  _id: string;
+  name: string;
+  image: string;
+  categoryType: string;
+}
+
+export interface Brand {
+  _id: string;
+  name: string;
+  image?: string;
+}
+
+export interface Product {
+  _id: string;
+  name: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  stock: number;
+  averageRating: number;
+  image: string;
+  category: Category;
+  brand: Brand;
+  ratings: [];
+  quantity?: number;
+}
+
+export interface Address {
+  _id: string;
+  street: string;
+  city: string;
+  country: string;
+  postalCode: string;
+  isDefault: boolean;
+}
+
+export interface AddressInput {
+  street: string;
+  city: string;
+  country: string;
+  postalCode: string;
+  isDefault?: boolean;
+}
+export type Banners = {
+  _id: string;
+  name: string;
+  title: string;
+  startFrom: number;
+  image: string;
+  bannerType: string;
+  createdAt: string;
+  updatedAt: string;
+};
