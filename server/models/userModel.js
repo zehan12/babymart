4b9925247b69b08294532d0bd35dcb7716566c73
zehan12 <zehan9211@gmail.com>
2025-11-21 13:14:50 +0530

    perf: optimize database queries for product listing

---
+++ server/models/userModel.js
@@ -0,0 +1,109 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/dlbqw7atu/image/upload/v1747734054/userImage_dhytay.png",
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user", "deliveryman"],
+      default: "user",
+    },
+    addresses: [
+      {
+        street: {
+          type: String,
+          required: true,
+        },
+        city: {
+          type: String,
+          required: true,
+        },
+        country: {
+          type: String,
+          required: true,
+        },
+        postalCode: {
+          type: String,
+          required: true,
+        },
+        isDefault: {
+          type: Boolean,
+          default: false,
+        },
+      },
+    ],
+    wishlist: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+    cart: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Encrypt password using bcrypt
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Ensure only one address is default
+userSchema.pre("save", async function (next) {
+  if (this.isModified("addresses")) {
+    const defaultAddress = this.addresses.find((addr) => addr.isDefault);
+    if (defaultAddress) {
+      this.addresses.forEach((addr) => {
+        if (addr !== defaultAddress) addr.isDefault = false;
+      });
+    }
+  }
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
