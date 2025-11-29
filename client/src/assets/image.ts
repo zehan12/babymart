4466c6c9f668496b256a770e58b7c77ca817bdbc
zehan12 <zehan9211@gmail.com>
2025-11-29 10:12:17 +0530

    feat: implement global search with search suggestions

---
+++ client/src/assets/image.ts
@@ -0,0 +1,17 @@
+import logo from "./images/logo.png";
+import smallLogo from "./images/smallLogo.png";
+import payment from "./images/payment.webp";
+import footerOne from "./images/footer/footerOne.svg";
+import footerTwo from "./images/footer/footerTwo.svg";
+import footerThree from "./images/footer/footerThree.svg";
+import footerFour from "./images/footer/footerFour.svg";
+
+export {
+  logo,
+  smallLogo,
+  payment,
+  footerOne,
+  footerTwo,
+  footerThree,
+  footerFour,
+};
