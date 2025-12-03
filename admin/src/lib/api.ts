8af20d46a2fa1c5a8fe1bad63a13310d395102e1
zehan12 <zehan9211@gmail.com>
2025-12-03 08:03:16 +0530

    feat: add administrative dashboard for order tracking

---
+++ admin/src/lib/api.ts
@@ -0,0 +1,12 @@
+export { default as api } from "./config";
+
+export {
+  getAdminApiConfig,
+  adminApi,
+  ADMIN_API_ENDPOINTS,
+  buildAdminQueryParams,
+} from "./config";
+
+// Export default for backward compatibility
+
+export { default } from "./config";
